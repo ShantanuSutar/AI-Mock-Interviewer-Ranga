3782df733359e6b11f4739ff008c4f0c4f6d6ca7
Shantanu Sutar <shantanusutar17@gmail.com>
2024-06-22 20:31:59 +0530

    creating interview schema

---
+++ drizzle.config.js
@@ -0,0 +1,8 @@
+/** @type { import("drizzle-kit").Config } */
+export default {
+  schema: "./utils/schema.js",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: "postgresql://shantanusutar17:PsnMzWCr40Rc@ep-lingering-darkness-a1f40ckt.ap-southeast-1.aws.neon.tech/AI%20Mock%20Interviewer%3A%20Ranga%20?sslmode=require",
+  },
+};
